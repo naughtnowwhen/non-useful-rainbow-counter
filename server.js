ff308242affc588fc3b9be7c3222da38fdd49389
Peter Murphy <naughtnowwhen@gmail.com>
2019-04-21 15:05:30 -0700

    added node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.listen(PORT, () => console.log(`listening on port : ${PORT}`));
 app.use(express.static('./public'));
